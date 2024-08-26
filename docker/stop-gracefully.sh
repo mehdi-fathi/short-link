60bed106a68f33262daabb03e45810de5aae7d8e
Mehdi <mehdifathi.developer@gmail.com>
2024-08-26 16:17:47 +0330

    Fix:fix bug stop-gracefully

--- docker/stop-gracefully.sh
+++ docker/stop-gracefully.sh
@@ -6,7 +6,7 @@ container_name="go_app"
 # Send SIGINT signal to the specific container
 echo "Sending SIGINT stop to container $container_name for stop app"
 
-docker compose exec app pkill -SIGINT main
+docker compose -f docker/docker-compose.yml --env-file .env.local exec app pkill -SIGINT main
 
 # Wait for a short period to allow containers to handle the SIGINT
 echo "Waiting for app to doing all process gracefully..."
@@ -14,4 +14,4 @@ sleep 30
 
 # Stop all containers
 echo "Stopping containers..."
-docker compose stop
+docker compose -f docker/docker-compose.yml --env-file .env.local stop
